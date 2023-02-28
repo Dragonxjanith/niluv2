29d2d3edf58143cc775d5b3c7b5f237bc7775cff
Queen NIlu <101912503+Dragonxjanith@users.noreply.github.com>
2023-02-28 20:40:33 +0530

    hgg

--- QueenNilu.js
+++ QueenNilu.js
@@ -7510,7 +7510,6 @@ case 'alive': case 'panel': case 'list': case 'menu': case 'help': case '?': {
             let ments = [ownernya, me, ini_mark]        
             let buttons = [{ buttonId: 'allmenu', buttonText: { displayText: 'All Menu' }, type: 1 },{ buttonId: 'command', buttonText: { displayText: 'List Menu' }, type: 1 },{ buttonId: 'sc', buttonText: { displayText: 'Script' }, type: 1 }]
             let buttonMessage = {
-  image:("Media/janiya/bot.jpg"),
   caption: xeonezy,
   footer: botname,
   buttons: buttons,
