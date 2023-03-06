3690ae295ce9af707f8015578a8243f7515bc185
Queen NIlu <101912503+Dragonxjanith@users.noreply.github.com>
2023-03-06 16:10:04 +0530

    eml

--- index.js
+++ index.js
@@ -160,57 +160,39 @@ async function startQueenNilu() {
                 var buffer = await getBuffer(ppuser)
                 let fgclink = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "6289523258649-1604595598@g.us"}, "message": {orderMessage: {itemCount: 9999999,status: 200, thumbnail: buffer, surface: 200, message: `${metadata.subject}`, orderTitle: 'memek', sellerJid: '0@s.whatsapp.net'}}, contextInfo: {"forwardingScore":999,"isForwarded":true},sendEphemeral: true}
                 he = `Welcome To ${metadata.subject} @${num.split("@")[0]}\n\n${metadata.desc}`
-                let link = `https://youtu.be/cvj3054O5NU`
+
 let buttons = [
 {buttonId: `halo`, buttonText: {displayText: 'WELCOME'}, type: 1}
 ]
 let buttonMessage = {
-document: fs.readFileSync('./lib/tes.xlsx'),
-mimetype: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-jpegThumbnail:buffer,
 mentions: [num],
 fileName: `Welcome To ${metadata.subject}`,
 fileLength: 99999999999999,
-caption: `${he} \n ${global.welcome}`,
-footer: `© DARK MAKER`,
+caption: `${global.welcome}`,
+footer: `Qᴜᴇᴇɴ ɴɪʟᴜ ꜱᴛᴀʙʟᴇ`,
 buttons: buttons,
 headerType: 4,
-contextInfo:{externalAdReply:{
-title: `Don't forget to smile today`,
-body: `SUBSCRIBE NIMA OFC`,
 mediaType:2,
-thumbnail: buffer,
-sourceUrl: link,
-mediaUrl: link,
-}}
+thumbnail: buffer
+
 }
+
 QueenNilu.sendMessage(anu.id, buttonMessage, {quoted:fgclink})
                 } else if (anu.action == 'remove') {
                     let fgclink = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "6289523258649-1604595598@g.us"}, "message": {orderMessage: {itemCount: 9999999,status: 200, thumbnail: buffer, surface: 200, message: `${metadata.subject}`, orderTitle: 'memek', sellerJid: '0@s.whatsapp.net'}}, contextInfo: {"forwardingScore":999,"isForwarded":true},sendEphemeral: true}
                     he = `He/She is gone bro ${metadata.subject} @${num.split("@")[0]}\n\n${metadata.desc}`
-                    let link = `https://youtu.be/cvj3054O5NU`
 let buttons = [
 {buttonId: `halo`, buttonText: {displayText: 'BYE'}, type: 1}
 ]
 let buttonMessage = {
-document: fs.readFileSync('./lib/tes.xlsx'),
-mimetype: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-jpegThumbnail:buffer,
 mentions: [num],
 fileName: `He/She is gone bro ${metadata.subject}`,
-fileLength: 99999999999999,
 caption: he,
-footer: `© DARK MAKER`,
+footer: `Qᴜᴇᴇɴ ɴɪʟᴜ ꜱᴛᴀʙʟᴇ`,
 buttons: buttons,
 headerType: 4,
-contextInfo:{externalAdReply:{
-title: `BYE DONT COME BACK HERE OKAY`,
-body: `SUBSCRIBE NIMA OFC`,
 mediaType:2,
-thumbnail: buffer,
-sourceUrl: link,
-mediaUrl: link,
-}}
+thumbnail: buffer
 }
 QueenNilu.sendMessage(anu.id, buttonMessage, {quoted:fgclink})
                 }
