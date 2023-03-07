14574f4c16aa3acb27cfd27e6f2ab546d1c6fe4b
Queen NIlu <101912503+Dragonxjanith@users.noreply.github.com>
2023-03-07 17:06:23 +0530

    ff

--- settings.js
+++ settings.js
@@ -79,10 +79,10 @@ global.mess = {
 }
 
 //media target
-global.thum = fs.readFileSync("./Media/janiya/bot.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./Media/janiya/bot.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./Media/janiya/bot.jpg") //ur error pic
-global.thumb = fs.readFileSync("./Media/janiya/bot.jpg") //ur thumb pic
+global.thum = fs.readFileSync("./Media/bot.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./Media/bot.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./Media/bot.jpg") //ur error pic
+global.thumb = fs.readFileSync("./Media/bot.jpg") //ur thumb pic
 
 //menu image maker
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
