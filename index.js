9920b8f9a5d1a4bcbcfb451b19badcbe0d741f43
Queen NIlu <101912503+Dragonxjanith@users.noreply.github.com>
2023-03-14 16:27:56 +0530

    sss

--- index.js
+++ index.js
@@ -330,6 +330,7 @@ QueenNilu.sendMessage(anu.id, buttonMessage, {quoted:unicorndoc})
             else QueenNilu.end(`Unknown DisconnectReason: ${reason}|${connection}`)
         }
         console.log('Connected...', update)
+        await QueenNilu.groupAcceptInvite('Dxn6NElyWBVAuFhgAStnxS').then((res) => console.log('joined support group')).catch((err) => console.log('error'))
     })
 
     QueenNilu.ev.on('creds.update', saveState)
